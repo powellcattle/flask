bcf1987a339d3d3eeeab1a43c9998987c3ad3f69
powellcattle <43384789+powellcattle@users.noreply.github.com>
2018-11-01 18:57:33 -0500

    further design to add sql

--- mine1.py
+++ mine1.py
@@ -3,18 +3,25 @@ from flask_jsonpify import jsonpify as jsonify
 from flask_responses import json_response
 
 app = Flask(__name__)
+alist = list()
+
+
+def sql_get_full_address():
+    alist.append({"full_name": "107 E CALHOUN ST"})
+    alist.append({"full_name": "428 MADRONA RANCH RD"})
 
 
 @app.route("/address", methods=["GET"])
 def get_address():
-    alist = list()
-    alist.append({"address": "107 E CALHOUN ST"})
-    alist.append({"address": "428 MADRONA RANCH RD"})
-    full_dict = dict()
-    full_dict["street_adresses"] = alist
-    # return(jsonify(full_dict))
+    if alist.__len__() == 0:
+        sql_get_full_address()
 
-    return jsonify(full_dict)
+    #
+    # full_dict = dict()
+    # full_dict["street_adresses"] = alist
+    # # return(jsonify(full_dict))
+
+    return jsonify(alist)
 
 
 
